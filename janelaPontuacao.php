9a281cbb6e5382621cddf84015c9d0cce7ea3299
matheus giacomelli dos santos <matheusgds66@gmail.com>
2021-05-22 18:36:06 -0300

    todas imagens implantadas, criado metodo javascript
    todas imagens estão aparecendo, todos valores dos dados, e os dados, mostrando pontuação de quem ganha perde e empate... finalizado

--- janelaPontuacao.php
+++ janelaPontuacao.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" type="text/css" href="css/estilo.css"/>
     </head>
     <body>
-        
+
         <div>
             <h2 id="divpontos"></h2>
         </div>
@@ -16,17 +16,38 @@
         <div name="divjogador" id="divjogador">
             <h1> PESSOA </h1>
             <div>
-
-
+                <img style="margin-left:10px" id="imgdado1jogador"> 
+                <h2 name ="h2dado1p" id="h2dado1p"></h2>
+                <img style="margin-left:10px" id="imgdado2jogador"> 
+                <h2 name ="h2dado2p" id="h2dado2p"></h2>
+                <br><br>
+                <h2 name ="h2dado2p" id="h2geraldadospessoa"></h2>
             </div>
         </div>
         <div name="divcomputador" id="divcomputador">
             <h1> COMPUTADOR </h1>
+            <div>
+                <img style="margin-left:10px" id="imgdado1computador"> 
+                <h2 name ="h2dado1c" id="h2dado1c"></h2>
+                <img style="margin-left:10px" id="imgdado2computador"> 
+                <h2 name ="h2dado2c" id="h2dado2c"></h2>
+                <br><br>
+                <h2 name ="h2dado2p" id="h2geraldadoscomputador"></h2>
 
+            </div>
         </div>
 
 
         <script type="text/javascript">
+            var dado1c = parseInt((Math.random() * (6 - 1) + 1), 10);
+            var dado2c = parseInt((Math.random() * (6 - 1) + 1), 10);
+            var dado1p = parseInt((Math.random() * (6 - 1) + 1), 10);
+            var dado2p = parseInt((Math.random() * (6 - 1) + 1), 10);
+
+            var valor1 = dado1c + dado2c;
+            var valor2 = dado1p + dado2p;
+
+
             function returnURLIMG(numero) {
                 switch (numero) {
                     case 1:
@@ -54,15 +75,6 @@
             }
 
             function pegarvalor() {
-                var dado1c = Math.random() * (6 - 1) + 1;
-                var dado2c = Math.random() * (6 - 1) + 1;
-                var dado1p = Math.random() * (6 - 1) + 1;
-                var dado2p = Math.random() * (6 - 1) + 1;
-
-                var valor1 = dado1c + dado2c;
-                var valor2 = dado1p + dado2p;
-
-
                 if (valor1 == valor2) {
                     document.getElementById("divpontos").innerHTML = "Empate!";
                 } else if (valor1 > valor2) {
@@ -71,7 +83,22 @@
                     document.getElementById("divpontos").innerHTML = "Pessoa Ganhou!";
                 }
             }
-            
+
+            function carregaimgs() {
+                document.getElementById("imgdado1jogador").src = returnURLIMG(dado1p);
+                document.getElementById("h2dado1p").innerHTML = dado1p;
+                document.getElementById("imgdado2jogador").src = returnURLIMG(dado2p);
+                document.getElementById("h2dado2p").innerHTML = dado2p;
+                document.getElementById("h2geraldadospessoa").innerHTML = dado1p + " + " + dado2p + " = " + valor2;
+
+                document.getElementById("imgdado1computador").src = returnURLIMG(dado1c);
+                document.getElementById("h2dado1c").innerHTML = dado1c;
+                document.getElementById("imgdado2computador").src = returnURLIMG(dado2c);
+                document.getElementById("h2dado2c").innerHTML = dado2c;
+                document.getElementById("h2geraldadoscomputador").innerHTML = dado1c + " + " + dado2c + " = " + valor1;
+            }
+
+            window.onload = carregaimgs();
             window.onload = pegarvalor();
         </script>
     </body>
