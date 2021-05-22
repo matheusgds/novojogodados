26c9281427b02e8d896c4741b48e1068f5002c52
matheus giacomelli dos santos <matheusgds66@gmail.com>
2021-05-22 15:57:44 -0300

    problema encontrado por nao escrever variavel do javascript no html, resolvendo problema
    problema em escrever a variavel javascript no html, na parte que diz document.getElementById("divpontos").innerHTML = "Pessoa Jogadora Ganhou!";

--- janelaPontuacao.php
+++ janelaPontuacao.php
@@ -1,40 +1,5 @@
 <!DOCTYPE html>
-<?php
 
-function returnURLIMG($i) {
-    switch ($i) {
-        case 1:
-            return "IMG/1.png";
-            break;
-        case 2:
-            return "IMG/2.png";
-            break;
-        case 3:
-            return "IMG/3.png";
-            break;
-        case 4:
-            return "IMG/4.png";
-            break;
-        case 5:
-            return "IMG/5.png";
-            break;
-        case 6:
-            return "IMG/6.png";
-            break;
-        default :
-            return "";
-            break;
-    }
-}
-
-$dado1c = rand(1, 6);
-$dado2c = rand(1, 6);
-$dado1p = rand(1, 6);
-$dado2p = rand(1, 6);
-
-$valor1 = (int) $dado1c + (int) $dado2c;
-$valor2 = (int) $dado1p + (int) $dado2p;
-?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -43,7 +8,7 @@ $valor2 = (int) $dado1p + (int) $dado2p;
         <link rel="stylesheet" type="text/css" href="css/estilo.css"/>
     </head>
     <body>
-        <script>
+        <script type="text/javascript">
             function returnURLIMG(numero) {
                 switch (numero) {
                     case 1:
@@ -70,53 +35,41 @@ $valor2 = (int) $dado1p + (int) $dado2p;
                 }
             }
 
-            var dado1c = Math.random() * (6 - 1) + 1;
-            var dado2c = Math.random() * (6 - 1) + 1;
-            var dado1p = Math.random() * (6 - 1) + 1;
-            var dado2p = Math.random() * (6 - 1) + 1;
+            function pegarvalor() {
+                var dado1c = Math.random() * (6 - 1) + 1;
+                var dado2c = Math.random() * (6 - 1) + 1;
+                var dado1p = Math.random() * (6 - 1) + 1;
+                var dado2p = Math.random() * (6 - 1) + 1;
+
+                var valor1 = dado1c + dado2c;
+                var valor2 = dado1p + dado2p;
+
+
+                if (valor1 == valor2) {
+                    document.getElementById("divpontos").innerHTML = "Empate!";
+                } else if (valor1 > valor2) {
+                    document.getElementById("divpontos").innerHTML = "Computador Ganhou!";
+                } else {
+                    document.getElementById("divpontos").innerHTML = "Pessoa Jogadora Ganhou!";
+                }
+            }
             
-            var valor1 = dado1c+dado2c;
-            var valor2 = dado1p+dado2p;
-            
+            window.onload = pegarvalor();
         </script>
         <div>
-            <h2> <?php
-                if ($valor1 == $valor2):
-                    echo "Empate";
-                elseif ($valor1 > $valor2):
-                    echo "Computador Ganhou!";
-                elseif ($valor1 < $valor2):
-                    echo "Pessoa Jogadora Ganhou!";
-                endif;
-                ?>
-            </h2>
+            <h2 id="divpontos">Pontuação: </h2>
         </div>
 
         <div name="divjogador" id="divjogador">
             <h1> PESSOA </h1>
             <div>
-                <img style="margin-left:10px" src=<?php echo returnURLIMG($dado1p); ?> >
-                <h2 name ="h2dado1p" id="h2dado1p"> <?php echo $dado1p; ?></h2>
 
-                <img style="margin-left:10px" src=<?php echo returnURLIMG($dado2p); ?> >
-                <h2 name ="h2dado2p" id="h2dado2p"><?php echo $dado2p; ?></h2>
-                <br><br>
-                <h2 name ="h2dado2p" id="h2dado2p"><?php echo $dado1p; ?>+<?php echo $dado2p; ?> = <?php echo $valor2; ?></h2>
 
             </div>
         </div>
         <div name="divcomputador" id="divcomputador">
             <h1> COMPUTADOR </h1>
-            <div>
-                <img style="margin-left:10px" src=<?php echo returnURLIMG($dado1c); ?> >
-                <h2 name ="h2dado1c" id="h2dado1c"> <?php echo $dado1c; ?></h2>
 
-                <img style="margin-left:10px" src=<?php echo returnURLIMG($dado2c); ?> >
-                <h2 name ="h2dado2c" id="h2dado2c"><?php echo $dado2c; ?> </h2>
-                <br><br>
-                <h2 name ="h2dado1c" id="h2dado1c"> <?php echo $dado1c; ?>+<?php echo $dado2c; ?> = <?php echo $valor1; ?></h2>
-
-            </div>
         </div>
 
 
